fcec8b803447c2af95ece3c4b7274947ce2e408f
ArthurMa <4406arthur@gmail.com>
2020-12-20 11:33:57 +0800

    add webhook log

--- pkg/webhook/webhook.go
+++ pkg/webhook/webhook.go
@@ -5,20 +5,22 @@ import (
 	"encoding/json"
 	"net/http"
 	"os"
+
+	"k8s.io/klog/v2"
 )
 
 type AzureDevOpsBuildRQ struct {
 	Def          Definition `json:"definition"`
 	Params       string     `json:"parameters"`
-	SourceBranch string     `json"sourceBranch`
+	SourceBranch string     `json:"sourceBranch"`
 }
 
 type Definition struct {
 	ID string `json:"id"`
 }
 
-//TODO: 此功能目前開發給azuredevops build pipeline使用, payload spec 有特規
 //Callback ...
+//TODO: 此功能目前開發給azuredevops build pipeline使用, payload spec 有特規
 func Callback(endpoint string, status bool, extraInfo string) error {
 	var rq AzureDevOpsBuildRQ
 	json.Unmarshal([]byte(extraInfo), &rq)
@@ -30,6 +32,7 @@ func Callback(endpoint string, status bool, extraInfo string) error {
 	rq.SourceBranch = os.Getenv("AZP_MAPPING_BRANCH")
 	jsonRQ, _ := json.Marshal(rq)
 
+	klog.Infof("webhook payload: %s %s %s", endpoint, string(jsonRQ))
 	req, err := http.NewRequest("POST", endpoint, bytes.NewBuffer(jsonRQ))
 	req.Header.Set("Content-Type", "application/json")
 	req.Header.Set("Authorization", "Basic "+os.Getenv("AZP_PAT"))
@@ -38,6 +41,9 @@ func Callback(endpoint string, status bool, extraInfo string) error {
 	resp, err := client.Do(req)
 	if err != nil {
 		return err
+	}
+	if resp.StatusCode > 400 {
+		klog.Infof("webhook resp status code error: %i", resp.StatusCode)
 	}
 	defer resp.Body.Close()
 	return nil
